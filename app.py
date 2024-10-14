1d8f6ec7706b711340d41849b777c3b7c4329601
sohamroyy0112 <onesohamroy@gmail.com>
2024-10-14 17:23:47 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,34 @@
+import zipfile
+import os
+
+
+if not os.path.exists('leukemia_classification_model_optimized.h5'):
+    with zipfile.ZipFile('leukemia_classification_model_optimized.zip', 'r') as zip_ref:
+        zip_ref.extractall()
+
+
+model = load_model('leukemia_classification_model_optimized.h5')
+
+
+
 import streamlit as st
 import tensorflow as tf
 from tensorflow.keras.models import load_model
 from PIL import Image
 import numpy as np
 
-# Load the trained model
+
 model = load_model('leukemia_classification_model_optimized.h5')
 
-# Function to preprocess images
+
 def preprocess_image(image):
     image = image.resize((64, 64))
     image = np.array(image)
-    image = image / 255.0  # Normalize the image
-    image = np.expand_dims(image, axis=0)  # Add batch dimension
+    image = image / 255.0  
+    image = np.expand_dims(image, axis=0) 
     return image
 
-# Streamlit app
+
 st.title("Leukemia Detection App")
 
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png", "bmp"])
@@ -26,10 +39,10 @@ if uploaded_file is not None:
     st.write("")
     st.write("Classifying...")
     
-    # Preprocess the image
+
     preprocessed_image = preprocess_image(image)
 
-    # Make a prediction
+
     prediction = model.predict(preprocessed_image)
 
     # Display result
